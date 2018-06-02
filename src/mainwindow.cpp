5c6f3ee4878f9e0fba532431f32d2804e9ebab6f
totoshka <plehunov.anton_9@mail.ru>
2018-06-02 12:59:20 +0300

    add fill comboBox resolution

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,6 +3,10 @@
 #include "controller_mst.h"
 #include "settings-mst.h"
 
+#include <QString>
+
+using namespace std;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -10,14 +14,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     ui->btn_next_2->hide();
-
+    ui->cb_resolution->hide();
+    ui->lw_inter->hide();
 }
 
-bool Contains(Xrandr_monitor xm, string s)
+bool Contains(vector<string> xm, string s)
 {
-    for(int i = 0; i < xm.resolutions.size(); i++)
+    for(int i = 0; i < xm.size(); i++)
     {
-        if(xm.resolutions[i] == s)
+        if(xm[i] == s)
         {
             return true;
         }
@@ -30,26 +35,63 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+void get_resolution(QComboBox *cb_resol, QListWidget *lw_inter)
+{
+    vector<Xrandr_monitor> xm = Settings_mst::parse_xrandr();
+    int xm_size = xm.size();
+    vector<string> resol;
+    bool flag = true;
+
+
+    if(xm_size == 1)
+    {
+        for(int i = 0; i < xm_size; i++)
+        {
+
+            int xm_resolv_size = xm[i].resolutions.size();
+            for(int n = 0; n < xm_resolv_size; n++)
+            {
+
+                flag = true;
+                for(int j = 0; j < xm_size - 1; j++)
+                {
+                    if(!(Contains(xm[j + 1].resolutions, xm[i].resolutions[n])))
+                    {
+                        flag = false;
+                    }
+                }
+
+                if(flag && !(Contains(resol, xm[i].resolutions[n])))
+                {
+                    resol.push_back(xm[i].resolutions[n]);
+                }
+            }
+            lw_inter->addItem(QString::fromStdString(xm[i].interface));
+        }
+    }
+    else
+    {
+        lw_inter->addItem(QString::fromStdString(xm[0].interface));
+        resol = xm[0].resolutions;
+    }
+
+
+
+    for(int i = 0; i < resol.size(); i++)
+    {
+        cb_resol->addItem(QString::fromStdString(resol[i]));
+    }
+
+}
+
+void MainWindow::on_btn_next_1_clicked()
 {
     ui->lbl_hello->hide();
     ui->btn_next_1->hide();
 
+    ui->lw_inter->show();
     ui->btn_next_2->show();
-    ui->cb_1->show();
-    ui->cb_2->show();
-    ui->cb_3->show();
     ui->cb_resolution->show();
 
-    vector<Xrandr_monitor> xm = Settings_mst::parse_xrandr();
-    ui->cb_1->text(xm[0].interface);
-    ui->cb_2->text(xm[1].interface);
-
-    for(int i = 0; i < xm[0].resolutions.size(); i++)
-    {
-        if(Contains(xm[1], xm[0].resolutions[i]))
-        {
-            ui->cb_resolution->addItem(xm[0].resolutions[i]);
-        }
-    }
+    get_resolution(ui->cb_resolution, ui->lw_inter);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -18,6 +18,10 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_btn_next_2_clicked();
+
+    void on_btn_next_1_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
