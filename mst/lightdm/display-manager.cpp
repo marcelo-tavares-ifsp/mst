4402d8c47e887349d7f0f7140e4223871180ca3b
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2018-08-10 10:19:36 +0300

    mst/lightdm/display-manager: Use QT logging facilities
    * mst/lightdm/display-manager.cpp, mst/lightdm/display-manager.h: Use
      QT logging facilities.

--- mst/lightdm/display-manager.cpp
+++ mst/lightdm/display-manager.cpp
@@ -1,6 +1,6 @@
 #include "display-manager.h"
 
-
+Q_LOGGING_CATEGORY(display_manager_category, "mst.dm")
 
 DisplayManager::DisplayManager(string config_path) : config_path(config_path) {
 
@@ -9,6 +9,7 @@ DisplayManager::DisplayManager(string config_path) : config_path(config_path) {
 static void _configure_x11() {
     if (system("xset -dpms") || system("xset s off"))
     {
+        qCritical(display_manager_category) << "Could not configure X11.";
         throw "Could not configure X11.";
     }
 }
@@ -17,6 +18,8 @@ void DisplayManager::start() {
     string lightdm_cmd = "/usr/sbin/lightdm --config " + config_path;
     if (system(lightdm_cmd.c_str()))
     {
+        qCritical(display_manager_category)
+                << "Could not start lightdm: " << lightdm_cmd.c_str();
         throw "Could not start lightdm: " + lightdm_cmd;
     }
     _configure_x11();
@@ -26,6 +29,8 @@ void DisplayManager::add_seat(int seat_number) {
     string lightdm_cmd = "/usr/bin/dm-tool add-local-x-seat " + seat_number;
     if (system(lightdm_cmd.c_str()))
     {
+        qCritical(display_manager_category)
+                << "Could not configure seats: " << lightdm_cmd.c_str();
         throw "Could not configure seats: " + lightdm_cmd;
     }
 }
