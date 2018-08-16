243d848b721276fd32400f0b53adef9a591b6963
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2018-08-16 12:53:01 +0300

    scripts/apl_backup.sh: Just remove 'xorg.conf'

--- scripts/apl_backup.sh
+++ scripts/apl_backup.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 main() {
     local mst_user="$1"
     local last_backup_date="$(ls -1 /var/lib/mst/backup/ | head -1)"
-    local xorg_backup="/var/lib/$last_backup_date/xorg.conf"
-    local getty_backup="/var/lib/$last_backup_date/getty@.service"
+    local xorg_backup="/var/lib/mst/backup/$last_backup_date/xorg.conf"
+    local getty_backup="/var/lib/mst/backup/$last_backup_date/getty@.service"
 
-    if [ -f "$xorg_backup" ]; then
-	cp "$xorg_backup" /etc/X11/xorg.conf
-    fi
+    [ -f "/etc/X11/xorg.conf" ] && rm "/etc/X11/xorg.conf"
+
+    # if [ -f "$xorg_backup" ]; then
+    # 	cp "$xorg_backup" /etc/X11/xorg.conf
+    # fi
 
     if [ -f "$getty_backup" ]; then
 	cp "$getty_backup" /lib/systemd/system/getty@.service
