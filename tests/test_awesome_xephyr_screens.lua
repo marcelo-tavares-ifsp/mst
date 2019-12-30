f60ba2c635ddc7cee957326639b74b80d6e06987
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2019-12-30 11:03:41 +0300

    xephyr_screens.lua.template: Fix template
    * templates/awesome/xephyr_screens.lua.template: Fix template
    * tests/test_awesome_xephyr_screens.lua: Update.

--- tests/test_awesome_xephyr_screens.lua
+++ tests/test_awesome_xephyr_screens.lua
@@ -1,7 +1,7 @@
 if is_screen_available(1) then
-    os.execute("sudo Xephyr -softCursor -ac -br
-                  -mouse  'evdev,5,device=/dev/input/by-path/mouse'
-                  -keybd  'evdev,,device=/dev/input/by-path/keyboard'
-                  -screen 640x480 :1
-                  &")
+    os.execute("sudo Xephyr -softCursor -ac -br"
+               .. " -mouse  'evdev,5,device=/dev/input/by-path/mouse'"
+               .. " -keybd  'evdev,,device=/dev/input/by-path/keyboard'"
+               .. " -screen 640x480 :1"
+               .. " &")
 end
