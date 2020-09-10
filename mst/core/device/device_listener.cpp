d7f5296f98dcf45301541828e59c85eb8f305897
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-09-10 20:42:35 +0300

    device_listener (_debug_print_devices): Remove
    * mst/core/device/device_listener.cpp (_debug_print_devices): Remove
      unused procedure.

--- mst/core/device/device_listener.cpp
+++ mst/core/device/device_listener.cpp
@@ -11,28 +11,6 @@ Device_listener::Device_listener(DEVICE_TYPE type)
     qRegisterMetaType<DEVICE_TYPE>();
 }
 
-// static methods ///////////////////////////////////////////////////////////////
-
-static void _debug_print_devices(DEVICE_TYPE type, QVector<QString>* devices)
-{
-    switch (type) {
-    case DEVICE_TYPE::KEYBOARD:
-        qInfo(input_device_listener_category()) << "KEYBOARD: ";
-        break;
-    case DEVICE_TYPE::MOUSE:
-        qInfo(input_device_listener_category()) << "MOUSE: ";
-        break;
-    case DEVICE_TYPE::USB:
-        qInfo(input_device_listener_category()) << "USB: ";
-        break;
-    }
-
-    for (auto d : *devices)
-    {
-        qInfo(input_device_listener_category()) << "     " << d;
-    }
-}
-
 void Device_listener::run()
 {
     QString device = nullptr;
