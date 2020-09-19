8485ff80776875c61bedf3ee153f1fd74ea7b5b4
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2020-09-19 22:22:18 +0300

    mst/core/components/xorg.cpp: Simplify

--- mst/core/components/xorg.cpp
+++ mst/core/components/xorg.cpp
@@ -1,5 +1,3 @@
-#include <sstream>
-
 #include "xorg.h"
 #include "../configuration.h"
 #include "../types/template.h"
@@ -14,40 +12,43 @@ Xorg::Xorg(Configuration& config) : Component(config)
 
 /* Config printers. */
 
-static void _print_monitors(ostream& os, const Configuration& config)
+static QString _make_monitors_section(const Configuration& config)
 {
     Template_manager* template_manager = Template_manager::get_instance();
     Template monitor_tpl = template_manager->get_template("xorg/Monitor");
     Template option_tpl  = template_manager->get_template("xorg/Option");
+    QString result = "";
     option_tpl.set("name", "LeftOf");
     for (int32_t idx = 0; idx < config.get_seat_count(); idx++) {
         monitor_tpl.set("index", QString::number(idx));
         if (idx < (config.get_seat_count() - 1)) {
             monitor_tpl.set("options",
                             option_tpl.set("value", QString::number(idx + 1))
-                    .substitute());
+                            .substitute());
         } else {
             monitor_tpl.set("options", "");
         }
-        os << monitor_tpl.substitute().toStdString();
+        result += monitor_tpl.substitute();
     }
+    return result;
 }
 
-static void _print_device(ostream& os, Configuration& config)
+static QString _make_device_section(Configuration& config)
 {
     Template tpl = Template_manager::get_instance()
             ->get_template("xorg/Device");
-
+    QString result = "";
     for (int32_t idx = 0; idx < config.get_seat_count(); idx++)
     {
         QString interface = config.get_seat(idx)->get_monitor().get_interface();
-        os << tpl.set("interface", interface)
-              .set("index", QString::number(idx))
-              .substitute().toStdString();
+        result += tpl.set("interface", interface)
+                .set("index", QString::number(idx))
+                .substitute();
     }
+    return result;
 }
 
-static void _print_screen(ostream& os, Configuration& config)
+static QString _make_screen_section(Configuration& config)
 {
     int total_width = int(config.get_seat_count()) * config.get_seat(0)->get_monitor().get_current_resolution().get_width();
     Template tpl = Template_manager::get_instance()
@@ -59,29 +60,30 @@ static void _print_screen(ostream& os, Configuration& config)
             .set("width",  QString::number(total_width))
             .set("height", QString::number(height));
 
-    os << tpl.substitute().toStdString();
+    return tpl.substitute();
 }
 
-static void _print_layout(ostream& os, Configuration& config)
+static QString _make_layout_section(Configuration& config)
 {
     Template tpl = Template_manager::get_instance()
             ->get_template("xorg/ServerLayout");
+    QString result = "";
     for (int32_t idx = 0; idx < config.get_seat_count(); idx++) {
-        os << tpl.set("seat_index", QString::number(idx))
-              .substitute().toStdString();
+        result += tpl.set("seat_index", QString::number(idx)).substitute();
     }
+    return result;
 }
 
 void Xorg::configure()
 {
-    std::stringstream os;
-    _print_monitors(os, config);
-    _print_device(os,   config);
-    _print_screen(os,   config);
-    _print_layout(os,   config);
+    QString xorg_conf = _make_monitors_section(config)
+            + _make_device_section(config)
+            + _make_screen_section(config)
+            + _make_layout_section(config);
+
     component_configuration.add(XORG_FILE,
                                 "/etc/X11/xorg.conf",
-                                Template(QString::fromStdString(os.str())));
+                                Template(xorg_conf));
 
     component_configuration.add(XINIT_RC_FILE,
                                 "{{home}}/.xinitrc",
