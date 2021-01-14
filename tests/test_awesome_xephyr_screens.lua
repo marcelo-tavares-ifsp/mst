1a2fc8b5d826eea4c8ed43cba50cdb423d56b7e2
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-01-14 09:30:46 +0300

    tests/test_awesome_xephyr_screens.lua: Fix
    * tests/test_awesome_xephyr_screens.lua: Fix.

--- tests/test_awesome_xephyr_screens.lua
+++ tests/test_awesome_xephyr_screens.lua
@@ -1,5 +1,5 @@
 if is_screen_available(1) then
-    os.execute("sudo Xephyr -softCursor -ac -br"
+    os.execute("sudo Xephyr -softCursor -ac -br -resizeable "
                .. " -mouse  'evdev,5,device=/dev/input/by-path/mouse'"
                .. " -keybd  'evdev,,device=/dev/input/by-path/keyboard'"
                .. " -screen 640x480 :1"
