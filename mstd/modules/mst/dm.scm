e1a4e62ecb917a2b2d4d975b0b8fe856b20bd588
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-02-22 21:49:17 +0300

    dm.scm (is-seat-used?): New procedure
    * mstd/modules/mst/dm.scm (is-seat-used?): New procedure.

--- mstd/modules/mst/dm.scm
+++ mstd/modules/mst/dm.scm
@@ -1,5 +1,9 @@
 (define-module (mst dm)
+  #:use-module (ice-9 regex)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 rdelim)
   #:export (add-seat
+	    is-seat-used?
 	    start-lightdm))
 
 (define *debug?* #f)
@@ -22,3 +26,15 @@
       pid)
      (else
       (error "Could not start the display manager")))))
+
+
+;; Check if a seat with ID is used.
+(define (is-seat-used? id)
+  (define regexp (format #f ".*(:~a).*" id))
+  (let loop ((p (open-input-pipe "who")))
+    (let ((line (read-line p)))
+      (if (eof-object? line)
+	  #f
+	  (if (string-match regexp line)
+	      #t
+	      (loop p))))))
