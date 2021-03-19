dc36d147c37ed8b3bb87f260f9e76e80b246daa5
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-03-19 16:57:53 +0300

    log.scm, dm.scm: Add the copyright
    * mstd/modules/mst/log.scm, mstd/modules/mst/dm.scm: Add the copyright
      notice.

--- mstd/modules/mst/dm.scm
+++ mstd/modules/mst/dm.scm
@@ -1,3 +1,30 @@
+;;; dm.scm -- Display manager procedures.
+
+;; Copyright (C) 2021 "AZ Company Group" LLC <https://gkaz.ru/>
+;; Copyright (C) 2021 Artyom V. Poptsov <a@gkaz.ru>
+;;
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains procedures interaction with the display
+;; manager.
+
+
+;;; Code:
+
 (define-module (mst dm)
   #:use-module (ice-9 regex)
   #:use-module (ice-9 popen)
