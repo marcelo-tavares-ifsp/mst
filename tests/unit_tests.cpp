23fa093e6f9d1c16d3379c256edc2b489c21a4aa
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-06-21 21:23:48 +0300

    tests/unit_tests.cpp: Fix path to the templates

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
 {
     //QApplication app(argc, argv);
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-    Template_manager::get_instance()->set_template_dir("../templates/");
+    Template_manager::get_instance()->set_template_dir(SRCDIR "/../templates/");
 
     Test_utils test_utils;
     Test_awesome test_awesome;
