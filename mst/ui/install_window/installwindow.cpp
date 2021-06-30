e5926d24472a019e1d55c6366a20ac0e61a076df
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-06-30 23:34:29 +0300

    mst/ui/install_window/installwindow.cpp: Bugfix
    * mst/ui/install_window/installwindow.cpp
      (InstallWindow::configure_seat): Bugfix: use proper seat IDs.

--- mst/ui/install_window/installwindow.cpp
+++ mst/ui/install_window/installwindow.cpp
@@ -51,8 +51,8 @@ void InstallWindow::show_page(int number)
 void InstallWindow::configure_seat(int seat_id)
 {
     qInfo(install_window_category()) << seat_id << " was selected";
-    mst->reset_devices(seat_id);
-    current_seat_id = seat_id;
+    mst->reset_devices(seat_id - 1);
+    current_seat_id = seat_id - 1;
     initial_listeners();
 }
 
