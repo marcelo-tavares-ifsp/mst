940c2c38a87e2bbb75937e07acf14a3029174e71
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-07-13 09:39:05 +0300

    mstd/modules/mst/device-listener.scm: Add comments

--- mstd/modules/mst/device-listener.scm
+++ mstd/modules/mst/device-listener.scm
@@ -1,3 +1,30 @@
+;;; device-listener.scm -- Listen to and handle device events.
+
+;; Copyright (C) 2021 "AZ Company Group" LLC <https://gkaz.ru/>
+;; Copyright (C) 2021 Artyom V. Poptsov <a@gkaz.ru>
+;;
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; This module contains procedures to handle connecting/disconnecting
+;; USB devices.
+
+
+;;; Code:
+
 (define-module (mst device-listener)
   #:use-module (udev udev)
   #:use-module (udev monitor)
@@ -76,3 +103,5 @@ in a CONFIG."
     (udev-monitor-start-scanning! udev-input-monitor)
     (while #t
            (sleep 1))))
+
+;;; device-listener.scm ends here.
