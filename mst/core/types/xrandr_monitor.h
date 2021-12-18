67ecf0c47176f59cb4e4c5f3a24b9c04ca93f8f4
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-12-18 13:28:53 +0300

    mst/core/types/xrandr_monitor.h: Add Doxygen comments

--- mst/core/types/xrandr_monitor.h
+++ mst/core/types/xrandr_monitor.h
@@ -4,11 +4,22 @@
 #include <QString>
 #include <QVector>
 
+/**
+ * @brief The XRandr_monitor class describes an XRandr monitor.
+ */
 class XRandr_monitor
 {
 public:
     XRandr_monitor();
+
+    /**
+     * @brief interface -- Interface that is used to connect the monitor.
+     */
     QString interface;
+
+    /**
+     * @brief resolutions -- Supported monitor resolutions.
+     */
     QVector<QString> resolutions;
 };
 
