f5e15f8d20de39909c79d976dc02e56b31b24e99
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2021-12-18 13:24:12 +0300

    mst/core/utilites/utilites: Remove unused code
    * mst/core/utilites/utilites.cpp (ltrim_x, rtrim_x): Remove.

--- mst/core/utilites/utilites.cpp
+++ mst/core/utilites/utilites.cpp
@@ -7,26 +7,3 @@ vector<string> split(string input, char separator) {
     result.push_back(input.substr(p + 1));
     return result;
 }
-
-/**
- * @brief ltrim -- trim a string from the beginning.  The input
- *                  string is modified.
- * @param s -- an input string.
- */
-static inline void ltrim_x(string &s) {
-    s.erase(s.begin(), find_if(s.begin(), s.end(), [](int ch) {
-        return ! isspace(ch);
-    }));
-}
-
-/**
- * @brief rtrim -- trim a string from the end.  The input string is modified.
- * @param s -- an input string.
- */
-static inline void rtrim_x(string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-        return ! isspace(ch);
-    }).base(), s.end());
-}
-
-
