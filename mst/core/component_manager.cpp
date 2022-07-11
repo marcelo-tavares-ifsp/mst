ad2ba63dcdd131a7f331dc778e00ff0612cf77ab
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-07-11 08:39:08 +0300

    mst/core: Add comments for some files
    * mst/core/component.cpp, mst/core/component.h,
      mst/core/component_manager.cpp, mst/core/component_manager.h,
      mst/core/configuration.cpp, mst/core/configuration.h,
      mst/core/mst.cpp, mst/core/mst.h,
      mst/core/platform.cpp, mst/core/platform.h,
      mst/core/template_manager.cpp, mst/core/template_manager.h: Add
      the file commentary and copyright.

--- mst/core/component_manager.cpp
+++ mst/core/component_manager.cpp
@@ -1,3 +1,24 @@
+/* component_manager.cpp -- MST component manager implementation.
+ *
+ * Copyright (C) 2018-2022 "AZ Company Group" LLC <https://gkaz.ru/>
+ * Copyright (C) 2018-2022 Artyom V. Poptsov <a@gkaz.ru>
+ *
+ * This file is part of MST.
+ *
+ * MST is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MST is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MST.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "component_manager.h"
 
 #include "core/template_manager.h"
--- mst/core/mst.cpp
+++ mst/core/mst.cpp
@@ -1,3 +1,24 @@
+/* mst.cpp -- Core MST class implementation.
+ *
+ * Copyright (C) 2018-2022 "AZ Company Group" LLC <https://gkaz.ru/>
+ * Copyright (C) 2018-2022 Artyom V. Poptsov <a@gkaz.ru>
+ *
+ * This file is part of MST.
+ *
+ * MST is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MST is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MST.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "mst.h"
 #include <QCollator>
 #include <QDateTime>
