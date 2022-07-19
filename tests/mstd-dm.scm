43eec53eeea86ae7eddfbdde28661c4f3625c793
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-07-19 12:55:35 +0300

    tests/mstd-dm.scm ("%make-command:add-seat"): Fix
    * tests/mstd-dm.scm ("%make-command:add-seat"): Use 'string-match'.

--- tests/mstd-dm.scm
+++ tests/mstd-dm.scm
@@ -1,12 +1,14 @@
 (use-modules (srfi srfi-64)
+             (ice-9 regex)
              (mst component lightdm))
 
 
 (test-begin "mstd-dm")
 
-(test-equal "%make-command:add-seat"
-  "/usr/bin/dm-tool add-local-x-seat 1"
-  (%make-command:add-seat 1))
+(test-assert "%make-command:add-seat"
+  (string-match
+   ".* add-local-x-seat 1"
+   (%make-command:add-seat 1)))
 
 
 (define result (test-runner-fail-count (test-runner-current)))
