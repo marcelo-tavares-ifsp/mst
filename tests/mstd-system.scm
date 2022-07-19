048c07bdf79b42f44153e50c635de306cc3560ae
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2022-07-19 10:41:34 +0300

    tests/mstd-system.scm ("%make-command:notify-send"): Fix
    * tests/mstd-system.scm ("%make-command:notify-send"): Use 'string-match'.

--- tests/mstd-system.scm
+++ tests/mstd-system.scm
@@ -1,4 +1,5 @@
 (use-modules (srfi srfi-64)
+             (ice-9 regex)
              (mst system))
 
 
@@ -11,9 +12,10 @@
   "sudo --user='alice' -- udisksctl mount --no-user-interaction --block-device /dev/some/device"
   (%make-command:mount "/dev/some/device" "alice"))
 
-(test-equal "%make-command:notify-send"
-  "DISPLAY=:0 /usr/bin/notify-send --urgency=critical --icon=error 'hello world'"
-  (%make-command:notify-send 0 "hello world"))
+(test-assert "%make-command:notify-send"
+  (string-match
+   "DISPLAY=:0 .* --urgency=critical --icon=error 'hello world'"
+   (%make-command:notify-send 0 "hello world")))
 
 (test-equal "%make-command:display-number->user"
   "who | grep '(:0)' | sed -re 's/^([^ ]+) +.* \\(:[0-9]+\\)/\\1/g' | head -1"
